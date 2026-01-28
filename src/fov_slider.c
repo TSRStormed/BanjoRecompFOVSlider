0c2d2a7661cfc1a0abc5b4a087f8d616402eceb5
TSRStormed <107441806+TSRStormed@users.noreply.github.com>
2026-01-27 20:44:27 -0700

    Refactor FOV setting into OnPlayerUpdate function

--- src/fov_slider.c
+++ src/fov_slider.c
@@ -17,20 +17,14 @@ typedef struct {
 
 extern TransitionInfo s_current_transition;
 
-RECOMP_PATCH void mainThread_entry(void *arg) { 
-    core1_init();
-    sns_write_payload_over_heap();
-
-    while (1) {
-        if (s_current_transition.model_ptr == assetcache_get(ASSET_467_MODEL_TRANSITION_FALLING_JIGGIES) || 
-            getGameMode() != GAME_MODE_3_NORMAL && 
-            getGameMode() != GAME_MODE_4_PAUSED || 
-            level_get() == LEVEL_D_CUTSCENE ||
-            map_get() == MAP_91_FILE_SELECT) {
-            viewport_setFOVy(40);
+RECOMP_HOOK ("func_80295C14") void OnPlayerUpdate(void) { 
+    if (s_current_transition.model_ptr == assetcache_get(ASSET_467_MODEL_TRANSITION_FALLING_JIGGIES) || 
+        getGameMode() != GAME_MODE_3_NORMAL && 
+        getGameMode() != GAME_MODE_4_PAUSED || 
+        level_get() == LEVEL_D_CUTSCENE ||
+        map_get() == MAP_91_FILE_SELECT) {
+			viewport_setFOVy(40);
         } else {
             viewport_setFOVy(recomp_get_config_double("number_option"));
         }
-        mainLoop();
-    }
 }
