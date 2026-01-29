6f3645e3947506eb2905740647a2fbfd7c6083b9
TSRStormed <107441806+TSRStormed@users.noreply.github.com>
2026-01-29 16:14:20 -0700

    Add transition states and update FOV logic

--- src/fov_slider.c
+++ src/fov_slider.c
@@ -15,16 +15,28 @@ typedef struct {
     s32 substate;
 } TransitionInfo;
 
+typedef enum {
+    TRANSITION_STATE_0_NONE,
+    TRANSITION_STATE_1_LOADING,
+    TRANSITION_STATE_2_BLACK_IN,
+    TRANSITION_STATE_3_BLACK_OUT,
+    TRANSITION_STATE_4_FADE_IN,
+    TRANSITION_STATE_5_FADE_OUT,
+    TRANSITION_STATE_6_LOADING_WHITE,
+    TRANSITION_STATE_7_WHITE_IN,
+    TRANSITION_STATE_8_WHITE_OUT
+} TransitionState;
+
 extern TransitionInfo s_current_transition;
 
 RECOMP_HOOK ("func_80295C14") void OnPlayerUpdate(void) { 
-    if (s_current_transition.model_ptr == assetcache_get(ASSET_467_MODEL_TRANSITION_FALLING_JIGGIES) || 
-        getGameMode() != GAME_MODE_3_NORMAL && 
-        getGameMode() != GAME_MODE_4_PAUSED || 
+
+    if (s_current_transition.state == TRANSITION_STATE_4_FADE_IN || s_current_transition.state == TRANSITION_STATE_5_FADE_OUT ||
+        getGameMode() != GAME_MODE_3_NORMAL && getGameMode() != GAME_MODE_4_PAUSED ||
         level_get() == LEVEL_D_CUTSCENE ||
         map_get() == MAP_91_FILE_SELECT) {
-			viewport_setFOVy(40);
-        } else {
-            viewport_setFOVy(recomp_get_config_double("number_option"));
-        }
+        viewport_setFOVy(40);
+    } else {
+        viewport_setFOVy(recomp_get_config_double("number_option"));
+    }    
 }
