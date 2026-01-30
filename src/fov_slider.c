c6e2333f7f2dad632f1d5ed7e2ea3f3832e4f2f9
TSRStormed <107441806+TSRStormed@users.noreply.github.com>
2026-01-29 17:20:35 -0700

    Refactor TransitionInfo and update FOV handling
    Refactor TransitionInfo structure and update OnPlayerUpdate logic to handle default FOV settings based on game state.

--- src/fov_slider.c
+++ src/fov_slider.c
@@ -5,38 +5,50 @@
 #include "recompconfig.h"
 
 typedef struct {
-    s32 unk0;
-    void *transistion_info;
+    u8 uid;
     u8 state;
-    void *model_ptr;
+    u8 next_state;
+    // u8 pad3[1];
+    f32 duration;
+    s32 model_index;
+    s32 anim_index;
+    f32 scale;
+}TransitionInfo;
+
+extern struct{
+    s32 unk0; //times drawn?
+    TransitionInfo *transistion_info;
+    u8 state;
+    //u8 pad9[3]; //not needed
+    void * model_ptr; //asset_ptr
     f32 rotation;
     f32 timer;
-    void *anctrl;
-    s32 substate;
-} TransitionInfo;
-
-typedef enum {
-    TRANSITION_STATE_0_NONE,
-    TRANSITION_STATE_1_LOADING,
-    TRANSITION_STATE_2_BLACK_IN,
-    TRANSITION_STATE_3_BLACK_OUT,
-    TRANSITION_STATE_4_FADE_IN,
-    TRANSITION_STATE_5_FADE_OUT,
-    TRANSITION_STATE_6_LOADING_WHITE,
-    TRANSITION_STATE_7_WHITE_IN,
-    TRANSITION_STATE_8_WHITE_OUT
-} TransitionState;
-
-extern TransitionInfo s_current_transition;
+    AnimCtrl *anctrl;
+    s32 substate; //times update called?
+} s_current_transition;
 
 RECOMP_HOOK ("func_80295C14") void OnPlayerUpdate(void) { 
 
-    if (s_current_transition.state == TRANSITION_STATE_4_FADE_IN || s_current_transition.state == TRANSITION_STATE_5_FADE_OUT ||
-        getGameMode() != GAME_MODE_3_NORMAL && getGameMode() != GAME_MODE_4_PAUSED ||
-        level_get() == LEVEL_D_CUTSCENE ||
-        map_get() == MAP_91_FILE_SELECT) {
+    bool force_default_fov = FALSE;
+
+    if (s_current_transition.transistion_info != NULL) {
+        if (s_current_transition.transistion_info->model_index == ASSET_467_MODEL_TRANSITION_FALLING_JIGGIES 
+            && s_current_transition.model_ptr != NULL) {
+            force_default_fov = TRUE;
+        }
+    }
+
+    if (getGameMode() != GAME_MODE_3_NORMAL && getGameMode() != GAME_MODE_4_PAUSED) {
+        force_default_fov = TRUE;
+    }
+
+    if (level_get() == LEVEL_D_CUTSCENE || map_get() == MAP_91_FILE_SELECT) {
+        force_default_fov = TRUE;
+    }
+
+    if (force_default_fov) {
         viewport_setFOVy(40);
     } else {
         viewport_setFOVy(recomp_get_config_double("number_option"));
-    }    
+    }
 }
